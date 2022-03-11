0d029c72ef485d900df2a3e4a6987aa6e8c92fda
Lucas Houska <ljhouska75@gmail.com>
2022-03-11 16:30:22 -0600

    added button functions for maths

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,17 +1,37 @@
 $(readyNow);
 
 function readyNow() {
-    $('#equals').on('click', handleEquals)
+    $('#equals').on('click', handleEquals);
+    $('#add').on('click', handleAdd);
+    $('#subtract').on('click', handleSubtract);
+    $('#multiply').on('click', handleMultiply);
+    $('#divide').on('click', handleDivide);
 }
 
-function handleEquals() {
     let numOne = $('#firstInput').val();
     let numTwo = $('#secondInput').val();
     let symbol = '';
 
+function handleEquals() {
     let objectToCalculate = {
         numOne: numOne,
         symbol: '',
         numTwo: numTwo
     }
+}
+
+function handleAdd() {
+    symbol = 'add';
+}
+
+function handleDivide() {
+    symbol = 'divide';
+}
+
+function handleMultiply() {
+    symbol = 'multiply';
+}
+
+function handleSubtract() {
+    symbol = 'subtract'
 }
